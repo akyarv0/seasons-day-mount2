55b0efa7096e8851a5ce4047da9d2fa72187b9d9
akyarv0 <akyarv0@gmail.com>
2024-02-02 14:41:31 +0300

    work

--- exercise2.js
+++ exercise2.js
@@ -75,7 +75,7 @@
 
 // do {
 //   toplam += sayi;
- 
+
 //   sayi+=2;
 // } while (sayi <= 20);
 // console.log(toplam);
@@ -122,7 +122,7 @@
 //   console.log(`New price ${price-=price*0.05} TL'dir`);
 
 // }
-//!ASAL SAYI ÖRNEĞİ 
+//!ASAL SAYI ÖRNEĞİ
 
 // function sayi() {
 //   let sayi =+prompt("Bir sayı giriniz.");
@@ -173,7 +173,7 @@
 //         }
 //         i--;
 //     }
-    
+
 //     return reversed.trim();
 // }
 // let inputSentence = "Merhaba Codeium kullanıcıları";
@@ -184,3 +184,41 @@
 //   const reversedSentence = sentence.split(' ').reverse().join(' ')
 //   return reversedSentence
 // }
+
+//? *****İN CLASS FUNCTİONS 31.01.2024****//
+
+selamla();
+//!hoisted function
+function selamla() {
+  console.log("Selam");
+}
+selamla();
+//!parametreli function: selamVer(selam,kişi) içerisindeki parametreleri function içine yazılır.
+function selamVer(selam, kisi) {
+  console.log(`${kisi} ${selam} `);
+}
+// selamVer("Ahmet", "Günaydın");
+
+function carp(num1, num2) {
+  // console.log(num1*num2)
+  // return num1*num2 olması gereken yazım bu
+  const result = num1 * num2;
+  return result;
+}
+// const sonuc = carp(3,6)
+// console.log(sonuc);//burada sonuc undefined çıkr çünkü function içinde return edilmedi.
+
+carp(2 * 5); // result içerde return edildi çalışır.
+
+//? **** FUNCTİON EXPRESSİON****//
+
+const yazdir = function (msg) {
+  msg = prompt("isminiz");
+  console.log(`  SELAM ${msg} `);
+};
+// yazdir();
+const usAl = function(taban,us){
+    return taban**us
+}
+const us = usAl(3,4)
+console.log(us);
